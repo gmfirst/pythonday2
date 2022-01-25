362ef681cb9ccfae5ad04c533895a550179820a6
John Doe <johndoe@example.com>
2022-01-25 10:45:31 +0700

    HELLO

---
+++ Car.py
@@ -0,0 +1,50 @@
+from atexit import register
+from turtle import color
+
+
+class Car:
+    # Properties (คุณสมบัติของคลาส)
+    color = "No color" 
+    brand = "No brand"
+    number_of_seats= 4
+    number_of_wheels = 4
+    maxspeed = 0
+    plate_id = ""
+
+    # Constructor
+    def __init__(self, color, brand, number_of_seats, number_of_wheels, maxspeed, plate_id):
+        self.color = color
+        self.brand = brand
+        self.number_of_seats = number_of_seats
+        self.number_of_wheels = number_of_wheels
+        self.maxspeed = maxspeed
+        self.plate_id = plate_id
+
+    # method
+    def setColor(self, x):
+        self.color = x
+
+    def setBrand(self, x):
+        self.brand = x
+
+    def setNumberOfSeats(self, x):
+        self.number_of_seats = x
+
+    def setNumberOfWheels(self, x):
+        self.number_of_wheels = x
+
+    def setMaxSpeed(self, x):
+        self.maxspeed = x
+
+    def setPlateID(self, x):
+        self.Plate_id = x
+
+    def printData(self):
+        print("Color: ", self.color)
+        print("Brand: ", self.brand)
+        print("Seats: ", self.number_of_seats)
+        print("wheels: ", self.number_of_wheels)
+        print("Speed: ", self.maxspeed)
+        print("Plate ID: ", self.plate_id)
+
+
---
+++ Example1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def show_message(msg):
+    print(msg)
+
+show_message("Hi Jupyter")
+
+
+# In[ ]:
+
+
+print ("Hello Python Notebook")
+print ("สวัสดีครับ")
+print (2**3)
+
+
+# In[ ]:
+
+
+print(1+2)
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'print')
+
+
+# In[8]:
+
+
+help(print)
+help(input)
+help(numpy)
+
